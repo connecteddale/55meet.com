b3c0118d4ea6fbde2350236fa21301474aff9226
Dale Williams <connectedworld@gmail.com>
2026-01-18 23:02:33 +0200

    feat(12-01): register sessions router in main app
    - Export sessions_router from routers package
    - Include sessions_router in FastAPI app

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -5,5 +5,6 @@ from app.routers.admin import router as admin_router
 from app.routers.images import router as images_router
 from app.routers.teams import router as teams_router
 from app.routers.members import router as members_router
+from app.routers.sessions import router as sessions_router
 
-__all__ = ["auth_router", "admin_router", "images_router", "teams_router", "members_router"]
+__all__ = ["auth_router", "admin_router", "images_router", "teams_router", "members_router", "sessions_router"]
